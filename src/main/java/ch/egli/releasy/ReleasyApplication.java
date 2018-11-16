fb9151e25775cef0f5e8cb1925b4d82ab0a80447
ch-egli <chrsitian.egli@gmx.net>
2018-11-16 19:25:13 +0100

    Initial commit

---
+++ src/main/java/ch/egli/releasy/ReleasyApplication.java
@@ -0,0 +1,12 @@
+package ch.egli.releasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleasyApplication.class, args);
+	}
+}
